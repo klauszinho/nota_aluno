ea9384e0cc7f9086fe2c3ecd28b6cd28045b8ac0
vulgo everton maozinha <128055802+klauszinho@users.noreply.github.com>
2024-08-27 09:29:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const nota_1 = 10;
+const nota_2 = 5;
+const nota_3 = 4;
+
+const soma_notas = nota_1 + nota_2 + nota_3;
+const media = soma_notas / 3;
+
+if (media >= 6) {
+  console.log('O aluno foi aprovado');
+} else if (media >= 4 && media < 6) {
+  console.log('Está de prova final');
+} else {
+  console.log('O aluno foi reprovado');
+}
